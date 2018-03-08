9705855ff0d408166622cd7231313f848ef91677
Lingaprahasam <prahas.programming@gmail.com>
2018-03-08 16:30:48 +0100

    updated mc

--- ros-mc/src/mc/src/mc.py
+++ ros-mc/src/mc/src/mc.py
@@ -4,15 +4,41 @@ from Adafruit_MotorHAT_Motors import Adafruit_MotorHAT, Adafruit_DCMotor
 import time
 import atexit
 
-def __init__(self):
-    # create a default object, no changes to I2C address or frequency
-    mh = Adafruit_MotorHAT(addr=0x60)
+# create a default object, no changes to I2C address or frequency
+mh = Adafruit_MotorHAT(addr=0x60)
 
-def turnOffMotors():
-    mh.getMotor(1).run(Adafruit_MotorHAT.RELEASE)
-    mh.getMotor(2).run(Adafruit_MotorHAT.RELEASE)
-    mh.getMotor(3).run(Adafruit_MotorHAT.RELEASE)
-    mh.getMotor(4).run(Adafruit_MotorHAT.RELEASE)
+def __init__(self):    
+    releaseMotor(1)
+    releaseMotor(2)
+    releaseMotor(3)
+    releaseMotor(4)
 
+def releaseMotor(num):
+    if (num < 1) or (num > 4):
+            raise NameError('Motor must be between 1 and 4 inclusive')
+    motor = mh.getMotor(num)
+    motor.run(Adafruit_MotorHAT.RELEASE)
 
+def brakeMotor(self, num):
+    if (num < 1) or (num > 4):
+            raise NameError('Motor must be between 1 and 4 inclusive')
+    motor = mh.getMotor(num)
+    motor.run(Adafruit_MotorHAT.BRAKE)
 
+def forwardMotor(self, num):
+    if (num < 1) or (num > 4):
+            raise NameError('Motor must be between 1 and 4 inclusive')
+    motor = mh.getMotor(num)
+    motor.run(Adafruit_MotorHAT.FORWARD)
+
+def backwardMotor(self, num):
+    if (num < 1) or (num > 4):
+            raise NameError('Motor must be between 1 and 4 inclusive')
+    motor = mh.getMotor(num)
+    motor.run(Adafruit_MotorHAT.BACKWARD)
+
+def setMotorSpeed(self, motornum, speed):
+    if (motornum < 1) or (motornum > 4):
+            raise NameError('Motor must be between 1 and 4 inclusive')
+    motor = mh.getMotor(motornum)
+    motor.setSpeed(speed)
