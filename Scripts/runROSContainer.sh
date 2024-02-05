fbbda5af2d70e1c9d31cebc302de2c27257a222d
Lingaprahasam <prahas.programming@gmail.com>
2024-02-05 23:28:38 +0100

    added username variable

--- Scripts/runROSContainer.sh
+++ Scripts/runROSContainer.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 # Define variables
+USER_NAME="rosadmin"
 IMAGE_NAME="ros-humble-custom"
 CONTAINER_NAME="ros2-container"
 
 [[ $(docker ps -al -f name=$CONTAINER_NAME --format '{{.Names}}') == $CONTAINER_NAME ]] ||
-docker run -i -t -v /home/rosadmin/ROSRobot/ros2_ws:/opt/ros2_ws  -d --name $CONTAINER_NAME $IMAGE_NAME
+docker run -i -t -v /home/$USER_NAME/ROSRobot/ros2_ws:/opt/ros2_ws -d --name $CONTAINER_NAME $IMAGE_NAME
 
 docker exec -it ros2-container /bin/bash
