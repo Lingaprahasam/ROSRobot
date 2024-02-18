bf145d0cbe54b78e71268a799e987255de8f59e3
Lingaprahasam <prahas.programming@gmail.com>
2024-02-18 21:12:23 +0100

    docker exec after tail is not working

--- Scripts/runROSContainer.sh
+++ Scripts/runROSContainer.sh
@@ -7,4 +7,5 @@ CONTAINER_NAME="ros2-container"
 [[ $(docker ps -al -f name=$CONTAINER_NAME --format '{{.Names}}') == $CONTAINER_NAME ]] ||
 docker run --user ros --network=host --ipc=host  --privileged --device=/dev/gpiomem0 -v /home/rosadmin/ROSRobot/ros2_ws:/opt/ros2_ws --name ros2-container ros-humble-custom:latest tail -f /dev/null
 
-docker exec -it ros2-container /bin/bash
+# Doing tail after running, the below command doesn't execute
+#docker exec -it ros2-container /bin/bash
