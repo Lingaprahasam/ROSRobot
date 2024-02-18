11183f577de588b15f013ff2610cd25e323ff9d3
Lingaprahasam <prahas.programming@gmail.com>
2024-02-18 21:12:54 +0100

    run container with rosrobot_ws

--- Scripts/runROSContainer_rosrobot_ws.sh
+++ Scripts/runROSContainer_rosrobot_ws.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 # Define variables
-USER_NAME="rosadmin"
 IMAGE_NAME="ros-humble-custom"
 CONTAINER_NAME="ros2-container"
 
 [[ $(docker ps -al -f name=$CONTAINER_NAME --format '{{.Names}}') == $CONTAINER_NAME ]] ||
 docker run --user ros --network=host --ipc=host  --privileged --device=/dev/gpiomem0 -v /home/rosadmin/ROSRobot/rosrobot_ws:/opt/rosrobot_ws --name ros2-container ros-humble-custom:latest tail -f /dev/null
 
-docker exec -it ros2-container /bin/bash
+# Doing tail after running, the below command doesn't execute
+#docker exec -it ros2-container /bin/bash
